d90a3435cc9ce7d61e5c494ccd7033754b078ac7
Fatihbykl <41392755+Fatihbykl@users.noreply.github.com>
2021-01-31 19:00:29 +0300

    Added Key and gate

---
+++ Assets/Scripts/CollectKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectKey : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //animasyon, anahtar alındı
+            gameManager.CollectedKey();
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int remainingKeyCount;
+    public GameObject keysObject;
+    public GameObject gate;
+
+    private void Start()
+    {
+        remainingKeyCount = keysObject.transform.childCount;
+    }
+
+    public void CollectedKey()
+    {
+        remainingKeyCount--;
+        if (remainingKeyCount <= 0)
+        {
+            gate.GetComponent<Animator>().Play("Open");
+        }
+    }
+}
