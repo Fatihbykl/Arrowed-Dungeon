61d19437db2573a7d78c9363c083772ba3be8d46
Fatih <fatih.baykal54@hotmail.com>
2023-11-30 21:42:44 +0300

    Added joystick for mobile and skills button

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject joystickObject;
+
+    private VisualElement m_JoystickBack;
+    private VisualElement m_JoystickHandle;
+    private Vector2 m_JoystickPointerDownPosition;
+    private Vector2 m_JoystickDelta; // Between -1 and 1
+
     public CharacterController controller;
     public float speed = 6;
     public float gravity = -9.81f;
@@ -21,6 +30,16 @@ public class CharacterMovement : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    private void OnEnable()
+    {
+        var root = joystickObject.GetComponent<UIDocument>().rootVisualElement;
+        m_JoystickBack = root.Q("JoystickBack");
+        m_JoystickHandle = root.Q("JoystickHandle");
+        m_JoystickHandle.RegisterCallback<PointerDownEvent>(OnPointerDown);
+        m_JoystickHandle.RegisterCallback<PointerUpEvent>(OnPointerUp);
+        m_JoystickHandle.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+    }
+
     void Update()
     {
         if (!isActive) { return; }
@@ -31,9 +50,13 @@ public class CharacterMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
 
         //walk
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        direction = new Vector3(horizontal, 0f, vertical).normalized;
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = m_JoystickDelta.x;
+        float vertical = m_JoystickDelta.y;
+        //Debug.Log(horizontal + " " + vertical);
+        direction = new Vector3(horizontal, 0f, -vertical).normalized;
+        Debug.Log($"{direction}");
         animator.SetFloat("directionMagnitude", direction.magnitude);
 
         if (direction.magnitude >= 0.1f)
@@ -45,5 +68,32 @@ public class CharacterMovement : MonoBehaviour
             controller.Move(direction.normalized * speed * Time.deltaTime);
         }
     }
+    void OnPointerDown(PointerDownEvent e)
+    {
+        m_JoystickHandle.CapturePointer(e.pointerId);
+        m_JoystickPointerDownPosition = e.position;
+    }
+
+    void OnPointerUp(PointerUpEvent e)
+    {
+        m_JoystickHandle.ReleasePointer(e.pointerId);
+        m_JoystickHandle.transform.position = Vector3.zero;
+        m_JoystickDelta = Vector2.zero;
+    }
+
+    void OnPointerMove(PointerMoveEvent e)
+    {
+        if (!m_JoystickHandle.HasPointerCapture(e.pointerId))
+            return;
+        var pointerCurrentPosition = (Vector2)e.position;
+        var pointerMaxDelta = (m_JoystickBack.worldBound.size - m_JoystickHandle.worldBound.size) / 2;
+        var pointerDelta = Clamp(pointerCurrentPosition - m_JoystickPointerDownPosition, -pointerMaxDelta,
+            pointerMaxDelta);
+        m_JoystickHandle.transform.position = pointerDelta;
+        m_JoystickDelta = pointerDelta / pointerMaxDelta;
+    }
+
+    static Vector2 Clamp(Vector2 v, Vector2 min, Vector2 max) =>
+        new Vector2(Mathf.Clamp(v.x, min.x, max.x), Mathf.Clamp(v.y, min.y, max.y));
 }
 
